f3ab5f1ac3376402d45e6c0bdb3255eea7271bbf
syed1006 <syedsalman1006@gmail.com>
2025-09-18 18:02:50 +0530

    added wrapper to use http request method to for json body request

---
+++ src/network/http-request-json.ts
@@ -0,0 +1,15 @@
+import { HttpRequestOptions } from "src/types";
+import { NetworkContext } from "./context";
+import { httpRequest } from "./http-request";
+
+export async function httpRequestJson(
+    context: NetworkContext,
+    options: HttpRequestOptions
+): Promise<[any, number]> {
+    const [response, status] = await httpRequest(context, options);
+    try {
+        return [JSON.parse(response), status];
+    } catch (e) {
+        throw new Error(`Invalid JSON returned (${status}): ${response}`);
+    }
+}
